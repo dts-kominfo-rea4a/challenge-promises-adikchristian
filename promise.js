6df68c2f8200c687bedb7d66fcde48e39cc8c167
Adi Christian <34901010+adikchristian@users.noreply.github.com>
2022-09-18 13:40:01 +0800

    modifikasi file promies.js

--- promise.js
+++ promise.js
@@ -1,7 +1,39 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if (emosi === null) {
+      reject("maaf terjadi error");
+    } else {
+      let countMarah = 0;
+      let countTidakMarah = 0;
+      promiseTheaterIXX().then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].hasil == "marah") {
+            countMarah++;
+          } else {
+            countTidakMarah++;
+          }
+        }
+      });
+      promiseTheaterVGC().then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].hasil == "marah") {
+            countMarah++;
+          } else {
+            countTidakMarah++;
+          }
+        }
+        if (emosi == "marah") {
+          resolve(countMarah);
+        } else {
+          resolve(countTidakMarah);
+        }
+      });
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
